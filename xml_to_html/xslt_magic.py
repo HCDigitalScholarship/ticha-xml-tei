041cb46635adf62a1e0f4537324105cfdff2897b
Ian Fisher <ianfisher45@gmail.com>
2017-04-12 13:30:33 -0400

    Resolve some issues with xslt_magic.py
    xslt_magic.py now runs without errors, but it still doesn't produce the
    desired paginated output.

--- xml_to_html/xslt_magic.py
+++ xml_to_html/xslt_magic.py
@@ -1,7 +1,46 @@
 #!/usr/bin/env python3
 import sys
 import os
-from lxml import etree
+from lxml import etree, sax
+
+class TEIPager(sax.ElementTreeContentHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.in_text = False
+        self.tag_stack = []
+
+    def startElementNS(self, ns_name, qname, attributes=None):
+        if qname == 'text':
+            self.in_text = True
+            super().startElementNS(ns_name, qname, attributes)
+            # this line never actually produces a <div class="printed_text_page"> element in the
+            # output
+            self.startElementNS((None, 'div'), 'div', {(None, 'class'):'printed_text_page'})
+        elif qname == 'pb':
+            self.closeAllTags()
+            self.reopenAllTags()
+        else:
+            if self.in_text:
+                self.tag_stack.append( (ns_name, qname, attributes) )
+            super().startElementNS(ns_name, qname, attributes)
+
+    def endElementNS(self, ns_name, qname):
+        if qname == 'text':
+            self.in_text = False
+            super().endElementNS((None, 'div'), 'div')
+            super().endElementNS(ns_name, qname)
+        elif qname != 'pb':
+            if self.in_text:
+                self.tag_stack.pop()
+            super().endElementNS(ns_name, qname)
+
+    def closeAllTags(self):
+        for ns_name, qname, _ in reversed(self.tag_stack):
+            super().endElementNS(ns_name, qname)
+
+    def reopenAllTags(self):
+        for ns_name, qname, attributes in self.tag_stack:
+            super().startElementNS(ns_name, qname, attributes)
 
 def xml_to_html(xml_data):
     html_root = xml_to_html_root(xml_data)
@@ -9,9 +48,11 @@ def xml_to_html(xml_data):
 
 def xml_to_html_root(xml_data):
     xml_root = etree.XML(xml_data)
+    handler = TEIPager()
+    sax.saxify(xml_root, handler)
     xslt_root = etree.parse('transform.xslt').getroot()
     transform = etree.XSLT(xslt_root)
-    return transform(xml_root)
+    return transform(handler.etree)
 
 if __name__ == '__main__':
     if 2 <= len(sys.argv) <= 3:
